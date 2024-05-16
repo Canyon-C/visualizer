891f8d4277a71bec15f87a01253c76edd754546c
Canyon <canyoncavicke@gmail.com>
2024-05-16 01:24:46 -0700

    ui files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import { BackgroundBeams } from "./ui/background-beams";
+import { poppins } from "./ui/fonts";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div>
-        <BackgroundBeams />
+    <main className="flex flex-col">
+      <div className="flex justiy-center items-center min-h-screen">
+        <BackgroundBeams className="z-0"/>
+        <p className={`${poppins.className} z-1 text-center w-full`}>Algorithm Visualizer</p>
+      
       </div>
+        <div className={`${poppins.className} flex justify-center items-center min-h-screen`}>
+          
+        </div>
     </main>
   );
 }
